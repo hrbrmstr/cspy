7b0de1f3f763663177bbe834c1dfb1ab54a8fc3d
hrbrmstr <bob@rud.is>
2019-03-06 08:57:54 -0500

    R package repo initialization complete

---
+++ R/crsspy-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/crsspy>
+#' - BugReports: <https://gitlab.com/hrbrmstr/crsspy/issues>
+#' 
+#' @md
+#' @name crsspy
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
