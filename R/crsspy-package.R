5b589dda74d9147ecb4384a5835fc7562495ca4f
hrbrmstr <bob@rud.is>
2019-03-06 22:49:21 -0500

    wip

--- R/crsspy-package.R
+++ R/crsspy-package.R
@@ -1,12 +1,13 @@
-#' ...
-#' 
+#' Content Security Policy Decomposer & Evaluator
+#'
 #' - URL: <https://gitlab.com/hrbrmstr/crsspy>
 #' - BugReports: <https://gitlab.com/hrbrmstr/crsspy/issues>
-#' 
+#' - Javadoc: <https://www.javadoc.io/doc/com.shapesecurity/salvation/2.6.0>
+#'
 #' @md
 #' @name crsspy
 #' @docType package
+#' @keywords internal
 #' @author Bob Rudis (bob@@rud.is)
-#' @import httr
-#' @importFrom jsonlite fromJSON
+#' @import rJava
 NULL
