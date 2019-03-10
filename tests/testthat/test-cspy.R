cad794775cdc1371a50a92dd2109868f93b3e24a
hrbrmstr <bob@rud.is>
2019-03-10 16:18:22 -0400

    https

--- tests/testthat/test-cspy.R
+++ tests/testthat/test-cspy.R
@@ -24,3 +24,11 @@ expect_is(allows_unsafe_inline_script(x1), "logical")
 expect_is(allows_unsafe_inline_script(x2), "logical")
 
 check_script_unsafe_inline(x2)
+
+
+structure(list(vulnerable = c(FALSE, TRUE), n = c(3122L, 2837L
+)), class = c("tbl_df", "tbl", "data.frame"), .Names = c("vulnerable",
+"n"), row.names = c(NA, -2L))
+
+library(waffle)
+
